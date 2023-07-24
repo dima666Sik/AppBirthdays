b5e7c1927414cc3992d2ff8a47a4df564c054c79
dima666Sik <leniviy.demon.1@gmail.com>
2023-07-24 14:34:21 +0300

    Model Layer. This is intermediate layer. Models can to inherited on different layers how dto.

---
+++ src/main/java/ua/birthdays/app/models/AboutFriend.java
@@ -0,0 +1,13 @@
+package ua.birthdays.app.models;
+
+public class AboutFriend {
+    private final String nameFriend;
+
+    public AboutFriend(String nameFriend) {
+        this.nameFriend = nameFriend;
+    }
+
+    public String getNameFriend() {
+        return nameFriend;
+    }
+}
