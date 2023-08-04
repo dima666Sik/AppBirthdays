c9b076d36539408d8930f461d7e69e1edaddeb02
dima666Sik <leniviy.demon.1@gmail.com>
2023-08-05 00:07:10 +0300

    update readme. And delete/ignore useless files.

--- src/main/java/ua/birthdays/app/domain/interfaces/AuthService.java
+++
@@ -1,9 +0,0 @@
-package ua.birthdays.app.domain.interfaces;
-
-import ua.birthdays.app.models.User;
-
-public interface AuthService {
-    boolean registration(String firstName, String lastName, String email, char[] password);
-    User authorization(String email, char[] password);
-
-}
--- src/main/java/ua/birthdays/app/domain/interfaces/MainFeaturesService.java
+++
@@ -1,8 +0,0 @@
-package ua.birthdays.app.domain.interfaces;
-
-import ua.birthdays.app.models.UserFriendsData;
-
-public interface MainFeaturesService {
-    boolean createUserFriendsData(UserFriendsData userFriendsData);
-
-}
